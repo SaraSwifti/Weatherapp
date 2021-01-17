9ae23b74b662af3398d86422b573a9b7bb5c571d
Sara Taylor <sara.swifti@gmail.com>
2021-01-16 19:08:15 -0500

    reorganizing functions

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,46 @@
+$(document).ready(function () {
+    $("#search-btn").on("click", function (event) {
+        event.preventDefault();
+        var cityName = [];
 
-//my api key from the system//
-apiKey = "50c71d66656725befd4b46281e12c1a4";
-//apiCall = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
+        function cityWeather() {
+            var city = $(this).attr("#search-btn");
+
+            //my api key from the system//
+            apiKey = "50c71d66656725befd4b46281e12c1a4";
+            var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+            var queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + apiKey;
+            // variables for dealing with the date and cycling through the functions
+            var todayDate = new Date();
+            var thisMonth = currentDate.getMonth() + 1;
+            console.log(thisMonth);
+            console.log(todayDate);
+        };
+    });
+
+
+    });
 // function for creating City weather data
 
 
 
 //button event that will run these functions
-//$("#search-btn").on("click", function (event) {
-// event.preventDefault();
-var cityName = "Denver";
+//
+
 //$("#city-text").val().trim();
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+/*var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
 $.ajax({
     url: queryURL,
     method: "GET"
 })
     // have the response data create the elements in the city page
     .then(function (response) {
-    
-        
+
+
         console.log(response);
         //variables for the JSON
         var cityNa = response.city.name;
-        var todayDate = response.list[0].dt_txt; 
+        var todayDate = response.list[0].dt_txt;
         var todayTemp = (response.list[0].main.temp-273.15);
         todayTemp = parseFloat(todayTemp);
         todayTemp = Math.round(todayTemp);
@@ -39,12 +56,12 @@ $.ajax({
         // now adding the elements and attributes for the values
        // var todayIcon = $("<img>");
        // todayIcon.attr("src", todayWeatherIcon);
-        var todayHeaderRow = $("<h1>").text(cityNa + " " + " " + todayDate); 
+        var todayHeaderRow = $("<h1>").text(cityNa + " " + " " + todayDate);
         var toTemp = $("<p>").text("Temp: " + todayTemp + " Degrees Celsius (because that it what the rest of the world uses:)");
         var toHum = $("<p>").text("Humidity: " + todayHum + "%");
         var toWind = $("<p>").text("Wind Speed: " + todayWind + " Km/hour.");
 
-        
+
         //putting the data in the correct boxes
         $("#today-here").append(todayHeaderRow);
         $("#today-here").append(toTemp, toHum, toWind);
@@ -56,27 +73,31 @@ $.ajax({
         // I need ot work on this
         var dayArray = [0,8,16,24,32,39];
             for (i = 1; i < dayArray.length; i++) {
-                var theDate = response.list[i].dt_txt; 
+                var theDate = response.list[i].dt_txt;
                 var theTemp = (response.list[i].main.temp-273.15);
                 var theHum = response.list[i].main.humidity;
                 var thDate = $("<h5>").text(theDate);
                 var thTemp = $("<p>").text("Temp: " + theTemp + "C");
                 var thHum = $("<p>").text("Humidity: " + theHum + "%");
-                thDate.addId("card-date");
-                thTemp.addId("card-temp");
-                thHum.addId("card-hum");
-                $("#card-body").append(thDate, thTemp, thHum);
+                thDate.addClass("card-title");
+                thTemp.addClass("card-text");
+                thHum.addClass("card-text");
+                $("card-body").append(thDate, thTemp, thHum);
             }
 
     });
 
+
+
+
 //making the 5 day card function
-      /*  function buildCard (response) {
+     /*  function buildCard (response) {
             var dayArray = [0,8,16,24,32,39];
             for (i = 1; i < dayArray.length; i++) {
-                var theDate = response.list[i].dt_txt; 
+                var theDate = response.list[i].dt_txt;
                 var theTemp = (response.list[i].main.temp-273.15);
                 var theHum = response.list[i].main.humidity;
+
                 var thDate = $("<h5>").text(theDate);
                 var thTemp = $("<p>").text("Temp: " + theTemp + "C");
                 var thHum = $("<p>").text("Humidity: " + theHum + "%");
@@ -85,7 +106,7 @@ $.ajax({
                 thHum.addId("card-hum");
                 $("#card-body").append(thDate, thTemp, thHum);
             };
-            
+
         };
 
 
@@ -107,7 +128,7 @@ $.ajax({
             //defining the variables for each city
             //var cityNa = response.
 
-            //creating and connecting the elements? 
+            //creating and connecting the elements?
 
 
             // Log the queryURL
@@ -117,4 +138,4 @@ $.ajax({
 
 /*I think this goes on the very end: code for clearing the text in the search button
 function clear() {
-$("#city-text").empty()*/
+$("#city-text").empty() */
