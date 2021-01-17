0d52118dde27f86007ae68327ce9344b4972f058
Sara Taylor <sara.swifti@gmail.com>
2021-01-16 23:16:42 -0500

    As close as I can get at this hour

--- assets/script.js
+++ assets/script.js
@@ -48,8 +48,8 @@ $("#search-btn").on("click", function (event) {
 
 
             // now adding the elements and attributes for the values
-            var icon = response.list[0].weather.icon;
-            var image = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + icon + ".png");
+            //var icon = response.list[0].weather.icon;
+            //var image = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + icon + ".png");
             var todayHeaderRow = $("<h1>").text("The Weather for " + cityName + " " + " " + fullDate);
             var toTemp = $("<p>").text("Temp: " + todayTemp + " Degrees Celsius (because that it what the rest of the world uses:)");
             var toHum = $("<p>").text("Humidity: " + todayHum + "%");
@@ -58,100 +58,45 @@ $("#search-btn").on("click", function (event) {
 
             //putting the data in the correct boxes
             $("#today-here").append(todayHeaderRow);
-            $("#today-here").append(image, toTemp, toHum, toWind);
+            $("#today-here").append(toTemp, toHum, toWind);
 
             //making the city button to save all the data
             var cityButton = $("<button>").text(cityName);
             cityButton.addClass("list-group-item");
             $("#city-list").append(cityButton);
+        });
+    $.ajax({
+        url: queryURL1,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+            //var identifyDate = new Date();
+            //identifyDate.setDate(identifyDate.getDate() +1);
+            //var dayArray = [0, 8, 16, 24, 32, 39];
+            for (i = 0; i < response.list.length; i++) {
+                //establishing forecastDate
+                var forecastDate = new Date(response.list[i].dt * 1000);
 
-            var dayArray = [0, 8, 16, 24, 32, 39];
-            for (i = 1; i < dayArray.length; i++) {
-                var theDate = response.list[dayArray].dt_txt;
-                var theTemp = (response.list[dayArray].main.temp - 273.15);
-                var theHum = response.list[dayArray].main.humidity;
-                var thDate = $("<h5>").text(theDate);
+                var theDate = $("<h5>").text(forecastDate);
+                var theTemp = response.list[i].main.temp;
+                var theHum = response.list[i].main.humidity;
+
                 var thTemp = $("<p>").text("Temp: " + theTemp + "C");
                 var thHum = $("<p>").text("Humidity: " + theHum + "%");
-                thDate.addClass("card-title");
-                thTemp.addClass("card-text");
-                thHum.addClass("card-text");
-                $("card-body").append(thDate, thTemp, thHum);
-            }
-
-
-
+                //theDate.addClass("card-title");
+                //thTemp.addClass("card-text");
+                // thHum.addClass("card-text");
+                $("#card").append(theDate, thTemp, thHum);
+                $("#fdf").append("#card");
+            }; 
+          
         });
-    //};
-    //displaycityWeather();
-});
+        //for the life of me cannot figure out why this is breaking everything when I try to clear the space
+         //function clear() {
+        //$("#city-text").empty();
+
+    });
 
 
-
-
-
-//button event that will run these functions
-//
-
-
-//var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
-
-
-
-// I need ot work on this
-
-
-
-
-
-//making the 5 day card function
-// function buildCard (response) {
-/*var dayArray = [0, 8, 16, 24, 32, 39];
-for (i = 1; i < dayArray.length; i++) {
-    var theDate = response.list[i].dt_txt;
-    var theTemp = (response.list[i].main.temp - 273.15);
-    var theHum = response.list[i].main.humidity;
-
-    var thDate = $("<h5>").text(theDate);
-    var thTemp = $("<p>").text("Temp: " + theTemp + "C");
-    var thHum = $("<p>").text("Humidity: " + theHum + "%");
-    thDate.addId("#card-date");
-    thTemp.addId("#card-temp");
-    thHum.addId("card-hum");
-    $("#card-body").append(thDate, thTemp, thHum);
-};
-
-                };
-
-
-
-
-
-
-
-
-                //call the functions for each box within this space passing the data from this function to the next
-
-
-
-        // Grabbing and storing the input on city name property value from the button
-
-        //today's weather function
-
-
-                    //defining the variables for each city
-                    //var cityNa = response.
-
-                    //creating and connecting the elements?
-
-
-                    // Log the queryURL
-
-
-
-
-/* I think this goes on the very end: code for clearing the text in the search button
- function clear() {
- $("#city-text").empty()
- */
 
